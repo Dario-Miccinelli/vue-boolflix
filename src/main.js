f6a9adecf3c62f5016444fe5bceb3ea44724c8f8
Dario-Miccinelli <poduzzo@hotmail.com>
2022-11-29 10:51:05 +0100

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+//Importazione di bootstrap 5.2
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+
 Vue.config.productionTip = false
 
 new Vue({
