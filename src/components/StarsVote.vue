516aead752f20e3dbec5c8baf6bfae54cc636bf2
Dario-Miccinelli <poduzzo@hotmail.com>
2022-11-30 12:56:06 +0100

    creato component stelline

---
+++ src/components/StarsVote.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class=" ">
+
+       
+
+
+
+
+
+
+
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "StarsVote",
+    components: {
+
+    },
+    props: {
+        
+
+    },
+    data() {
+        return {
+           
+
+        }
+    },
+    methods: {
+
+    }
+
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
